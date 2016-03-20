07e26c4893c3fea0713ec7d3799d3a5ee4e87e79
Ian Neal <ian.gl.neal@gmail.com>
2016-03-20 15:35:40 -0500

    Added configuration files to the setup process

--- setup.sh
+++ setup.sh
@@ -11,4 +11,15 @@ for dotfile in dot/*; do
         ln -s $(pwd)/$dotfile ~/.$newname
     fi
 done
+###
+#   Routes program specific configurations to their correct locations
+###
+for config in config/*; do
+    if [ -f "$config" ]
+    then
+        dirname=${config:7}
+        rm -f ~/.$dirname/config
+        ln -s $(pwd)/$config ~/.$dirname/config
+    fi
+done
 echo "Done. Please restart your shell for bash configurations to appear"
