c0fb63ef4a3c474d002ae6a081aace7256b5cd23
Ian Glen Neal <ian.gl.neal@gmail.com>
2016-08-05 20:22:32 -0700

    Trying to get the local server version of textbelt up and running

--- shell/texter_setup.sh
+++ shell/texter_setup.sh
@@ -1,5 +1,29 @@
 #! /bin/bash
 
-cd ~/
-rm -rf .textbelt_server
-git clone https://github.com/typpo/textbelt.git .textbelt_server
+repo=~/.textbelt_server
+
+rm -rf $repo
+git clone https://github.com/Dahca/textbelt.git $repo
+
+cd $repo
+
+npm install
+
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make -j4
+#make -j4 test
+
+if [ "$(uname)" == "Darwin" ]; then
+  # Do something under Mac OS X platform
+  brew install mutt
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  # Do something under GNU/Linux platform
+  sudo apt-get install -y mutt
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+  # Do something under Windows NT platform
+  echo "Cannot use apt-get or homebrew under cygwin"
+fi
+
+
